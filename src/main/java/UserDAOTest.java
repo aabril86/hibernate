73892661572276059173e1654b3cac7082140955
abril <aabril@elpuig.xeill.net>
2021-03-19 18:35:49 +0100

    Initial commit

---
+++ src/main/java/UserDAOTest.java
@@ -0,0 +1,25 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class UserDAOTest {
+
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("usersdb");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        User newUser = new User();
+        newUser.setEmail("johndoe@elpuig.xeill.net");
+        newUser.setFullname("John Doe");
+        newUser.setPassword("johndoe");
+
+        entityManager.persist(newUser);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        factory.close();
+    }
+}
